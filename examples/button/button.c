c63a4f8fa80ddeeda1ad4d9b6372b5478fcf1e52
coevoe <85091338+KryonicNapkin@users.noreply.github.com>
2025-03-05 18:19:14 +0100

    Added examples

--- examples/button/button.c
+++ examples/button/button.c
@@ -1,20 +1,30 @@
+/* NOTE: You MUST #include "raylib.h" before RUIES_IMPLEMENTATION for the library to work */
 #include "raylib.h"
 
-#include "../../rlui_elems.h"
+/* Library and its implementation */
+#define RUIES_IMPLEMENTATION
+#include "../../ruies.h"
 
 int main(void) {
+    /* Standard raylib initialization */
     InitWindow(800, 600, "Test");
     SetTargetFPS(60);
 
-    set_elem_font_size(BUTTON, 20);
+    /* NOTE: For text to be displayed you need to call ruies_load_default_font() before any other function */
+    ruies_load_default_font();
 
-    Rectangle button_dims = {100, 100, 200, 50};
-    Button_t button = make_button(button_dims, "Click me");
+    /* Button dimensions as of type Ruies_Rect_t */
+    Ruies_Rect_t button_dims = {100, 100, 200, 50};
+    /* Creation of button */
+    Ruies_Button_t button = make_button(button_dims, "Click me");
 
+    /* Raylib loop */
     while (!WindowShouldClose()) {
         BeginDrawing();
         {
             ClearBackground(RAYWHITE);
+            /* Render the button */
+            /* NOTE: you pass it as a pointer because this function also changes the button state structure element */
             render_button(&button);
         }
         EndDrawing();
